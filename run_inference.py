1c63ff04047c3b89b867930d2b3510f84168c821
eddudos <eddudos.db@gmail.com>
2023-12-22 14:36:50 +0700

    update

--- run_inference.py
+++ run_inference.py
@@ -15,8 +15,6 @@ from omegaconf import OmegaConf
 from PIL import Image
 
 ##
-
-print("Using", torch.cuda.device_count(), "GPUs!")
 save_memory = True
 disable_verbosity()
 if save_memory:
@@ -27,12 +25,11 @@ config = OmegaConf.load('./configs/inference.yaml')
 model_ckpt =  config.pretrained_model
 model_config = config.config_file
 
-device = torch.device("cuda:1")
+device = torch.device("cuda")
 model = create_model(model_config ).cpu()
 model.load_state_dict(load_state_dict(model_ckpt, location='cuda'))
 model= torch.nn.DataParallel(model)
 model.to(device)
-
 ddim_sampler = DDIMSampler(model)
 
 
@@ -205,7 +202,7 @@ def inference_single_image(ref_image, ref_mask, tar_image, tar_mask, guidance_sc
     samples, intermediates = ddim_sampler.sample(ddim_steps, num_samples,
                                                     shape, cond, verbose=False, eta=eta,
                                                     unconditional_guidance_scale=scale,
-                                                    unconditional_conditioning=un_cond).to(device)
+                                                    unconditional_conditioning=un_cond)
     if save_memory:
         model.low_vram_shift(is_diffusing=False)
 
@@ -249,7 +246,11 @@ if __name__ == '__main__':
     tar_mask = cv2.imread(bg_mask_path)[:,:,0] > 128
     tar_mask = tar_mask.astype(np.uint8)
     
+    import time 
+    start = time.time()
     gen_image = inference_single_image(ref_image, ref_mask, back_image.copy(), tar_mask)
+    print(time.time() - start, 111111111)
+    
     h,w = back_image.shape[0], back_image.shape[0]
     ref_image = cv2.resize(ref_image, (w,h))
     vis_image = cv2.hconcat([ref_image, back_image, gen_image])
